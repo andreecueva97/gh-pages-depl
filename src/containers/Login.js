defff4dad8b258b9629a3e1a3a077c50d801a56c
andreecueva97 <andreecueva6@gmail.com>
2021-09-02 16:10:48 -0300

    changes css pages

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -49,7 +49,13 @@ function Login(props) {
   };
   
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
+    <div className="contenedor">
+      <header className="header">
+        <h2>HEADER</h2>
+      </header>
+
+      <div className="contenido"> 
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
       <h1 className='f1'>Login</h1>
       <form>
         <FormInput name='email' type='email' placeholder='email' handleChange={handleChange} required/>
@@ -59,6 +65,30 @@ function Login(props) {
       <button type="button" onClick={() => history.push("/")}>Go to Home</button>
       <button type="button" onClick={() => history.push("/register")}>Go to Register</button>
     </div>
+        
+      </div>
+      <div className="sidebar">
+        {/* <h1>SIDEBAR</h1> */}
+       
+        <h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        <br></br>
+        </h3>
+      </div>
+      <div className="widget1">
+        <h3>widget1</h3>
+      </div>
+      <div className="widget2">
+        <h3>widget2</h3>
+      </div>
+      <footer className="footer">
+        <h3>footer</h3>
+      </footer>
+      
+    </div>
+
+
+    
   );
 }
 
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -28,7 +28,13 @@ function Profile(props) {
   let history = useHistory();
   
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
+    <div className="contenedor">
+      <header className="header">
+        <h2>HEADER</h2>
+      </header>
+
+      <div className="contenido"> 
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
       <button type="button" onClick={() => history.push("/")}>Go Home</button>
       <h1 className='f1'>Profile</h1>
       <h2>id: {props.id}</h2>
@@ -38,6 +44,29 @@ function Profile(props) {
       <h2>Password: {props.password}</h2>
       <h2>IsPending: {(props.isPending).toString()}</h2>
     </div>
+        
+      </div>
+      <div className="sidebar">
+        {/* <h1>SIDEBAR</h1> */}
+       
+        <h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        <br></br>
+        </h3>
+      </div>
+      <div className="widget1">
+        <h3>widget1</h3>
+      </div>
+      <div className="widget2">
+        <h3>widget2</h3>
+      </div>
+      <footer className="footer">
+        <h3>footer</h3>
+      </footer>
+      
+    </div>
+
+    
   );
 }
 
